2a0728250fa6ac7f6f41c69eaf754a5c5aecb397
caseycloud12 <caseycloud12@gmail.com>
2026-09-05 16:30:56 -0500

    Replace Whirlwind with Aeroblast in Lugia learnset gen7

--- src/data/pokemon/level_up_learnsets/gen_7.h
+++ src/data/pokemon/level_up_learnsets/gen_7.h
@@ -7235,7 +7235,7 @@ static const struct LevelUpMove sTyranitarLevelUpLearnset[] = {
 
 #if P_FAMILY_LUGIA
 static const struct LevelUpMove sLugiaLevelUpLearnset[] = {
-    LEVEL_UP_MOVE( 1, MOVE_WHIRLWIND),
+    LEVEL_UP_MOVE( 1, MOVE_AEROBLAST),
     LEVEL_UP_MOVE( 1, MOVE_WEATHER_BALL),
     LEVEL_UP_MOVE( 9, MOVE_GUST),
     LEVEL_UP_MOVE(15, MOVE_DRAGON_RUSH),
